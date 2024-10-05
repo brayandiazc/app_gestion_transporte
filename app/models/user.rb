f02f7b98ee6197b909719fd8052c0dbf82e9194b
Brayan Diaz C <brayandiazc@gmail.com>
2024-10-05 09:54:45 -0500

    faker

--- app/models/user.rb
+++ app/models/user.rb
@@ -20,7 +20,7 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
 
   # Relaciones
-  has_many :trayectos
+  has_many :trayectos, dependent: :destroy
 
   # Logica enum
   enum tipo_usuario: { conductor: 'conductor', supervisor: 'supervisor' }
